1267331020f9c97e298697418e95b2fc0bc8a4e1
stella <sftamesis13@gmail.com>
2022-10-06 12:27:39 -0700

    first deliverable complete: retrieve toy data

--- src/components/ToyContainer.js
+++ src/components/ToyContainer.js
@@ -1,7 +1,18 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import ToyCard from "./ToyCard";
 
 function ToyContainer() {
+  const [toys, setToys] = useState([])
+  
+  useEffect(() => {
+    fetch("http://localhost:3001/toys")
+      .then(response => response.json())
+      .then(toyList => setToys(toyList))
+  }, [])
+
+  console.log({toys})
+
+
   return (
     <div id="toy-collection">{/* Render the collection of ToyCards */}</div>
   );
