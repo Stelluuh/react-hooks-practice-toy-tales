8d91b566d9d44b849a9b2900332c7d149e4210fc
stella <sftamesis13@gmail.com>
2022-10-07 09:30:54 -0700

    relocate fetch to grab data to APP instead of ToyContainer

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 import Header from "./Header";
 import ToyForm from "./ToyForm";
@@ -6,6 +6,15 @@ import ToyContainer from "./ToyContainer";
 
 function App() {
   const [showForm, setShowForm] = useState(false);
+  const [toys, setToys] = useState([])
+  
+  useEffect(() => {
+    fetch("http://localhost:3001/toys")
+      .then(response => response.json())
+      .then(toyList => setToys(toyList))
+  }, [])
+
+  console.log(toys)
 
   function handleClick() {
     setShowForm((showForm) => !showForm);
--- src/components/ToyContainer.js
+++ src/components/ToyContainer.js
@@ -1,18 +1,8 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import ToyCard from "./ToyCard";
 
 function ToyContainer() {
-  const [toys, setToys] = useState([])
   
-  useEffect(() => {
-    fetch("http://localhost:3001/toys")
-      .then(response => response.json())
-      .then(toyList => setToys(toyList))
-  }, [])
-
-  console.log({toys})
-
-
   return (
     <div id="toy-collection">{/* Render the collection of ToyCards */}</div>
   );
